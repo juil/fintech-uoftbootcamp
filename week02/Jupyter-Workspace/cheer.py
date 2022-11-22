43c5c29a7fe78a126945a94081bb18795f6c4c57
jiy.juil@gmail.com <jiy.juil@gmail.com>
2022-11-21 20:42:26 -0500

    Add cheer and for loop

--- week02/Jupyter-Workspace/cheer.py
+++ week02/Jupyter-Workspace/cheer.py
@@ -1,12 +1,13 @@
 # Create a variable named cheer and give it a word to cheer (i.e. Python or FinTech)
-
+cheer = 'Fintech'
 
 # Below strings can be used to add fun
 cheer_symbol = "*\O/*"
 cheer_symbol_2 = "ヘ( ^o^)ノ＼(^_^ )"
 
 # Loop through string and print each letter with a cheer
-
+for letters in cheer:
+    print(f'{letters}   {cheer_symbol}')
 
 # Print excitement to screen
 print("\nWhat does that spell?!")
