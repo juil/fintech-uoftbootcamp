04fc3df7ae5db4ef46de7119de8602a550abfbf2
jiy.juil@gmail.com <jiy.juil@gmail.com>
2022-11-21 20:38:28 -0500

    Add activity 4 sample file

---
+++ week02/Jupyter-Workspace/cheer.py
@@ -0,0 +1,15 @@
+# Create a variable named cheer and give it a word to cheer (i.e. Python or FinTech)
+
+
+# Below strings can be used to add fun
+cheer_symbol = "*\O/*"
+cheer_symbol_2 = "ヘ( ^o^)ノ＼(^_^ )"
+
+# Loop through string and print each letter with a cheer
+
+
+# Print excitement to screen
+print("\nWhat does that spell?!")
+print(cheer + "!\nWoohoo! Go " + cheer + "!")
+print(cheer_symbol * 3)
+print(cheer_symbol_2)
