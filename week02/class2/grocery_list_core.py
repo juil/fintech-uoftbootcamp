9fd024bd4620522494a3cd605f18df649c4c44db
jiy.juil@gmail.com <jiy.juil@gmail.com>
2022-11-22 19:24:49 -0500

    Copy activity 2 starter file.

---
+++ week02/class2/grocery_list_core.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+Student Do: Grocery List.
+
+This script showcases basic operations of Python Lists to help Mike
+organize his grocery shopping list.
+"""
+
+# @TODO: Create a list of groceries
+
+
+
+
+
+# @TODO: Find the first two items on the list
+
+
+
+
+# @TODO: Find the last five items on the list
+
+
+
+
+# @TODO: Find every other item on the list, starting from the second item
+
+
+
+
+# @TODO: Add an element to the end of the list
+
+
+
+
+
+# @TODO: Changes a specified element within the list at the given index
+
+
+
+
+
+# @TODO: Calculate how many items you have in the list
+
+
