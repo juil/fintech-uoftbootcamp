96e6cfb8e769e1b14b2d722e9eb791f26c9b6a45
jiy.juil@gmail.com <jiy.juil@gmail.com>
2022-11-22 20:27:21 -0500

    Fix dictonary

--- week02/class2/market_cap_challenge.py
+++ week02/class2/market_cap_challenge.py
@@ -26,20 +26,7 @@ bank names associated with the corresponding market cap ranges.
 
 # @TODO: Initialize a dictionary of banks and market caps (in billions)
 
-bank_mc = {National Bank of Canada: 327, Toronto-Dominion Bank: 302, Royal Bank of Canada: 173, Wells Fargo: 273, Goldman Sachs: 87, Morgan Stanley: 72, Canadian Imperial Bank of Commerce: 83,  TD Bank: 108, Bank of Montreal: 67, Capital One: 47, FNB Corporation: 4, Laurentian Bank of Canada: 3, Ally Financial: 12, Montreal Trust Company: 145, Canadian Western Bank: .97}
-
-
-
-
-
-
-
-
-
-
-
-
-
+bank_mc = {'National Bank of Canada': 327, 'Toronto-Dominion Bank': 302, 'Royal Bank of Canada': 173, 'Wells Fargo': 273, 'Goldman Sachs': 87, 'Morgan Stanley': 72, 'Canadian Imperial Bank of Commerce': 83,  'TD Bank': 108, 'Bank of Montreal': 67, 'Capital One': 47, 'FNB Corporation': 4, 'Laurentian Bank of Canada': 3, 'Ally Financial': 12, 'Montreal Trust Company': 145, 'Canadian Western Bank': .97}
 
 # @TODO: Change the market cap for 'Royal Bank of Canada'
 
