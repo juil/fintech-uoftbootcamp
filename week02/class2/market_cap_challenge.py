d7404dc7c690e95d72446bb88e39f1f6a389870f
jiy.juil@gmail.com <jiy.juil@gmail.com>
2022-11-22 19:57:51 -0500

    Add files for activity 4 & 6

---
+++ week02/class2/market_cap_challenge.py
@@ -0,0 +1,105 @@
+# -*- coding: utf-8 -*-
+"""
+Student Activity: Market Capitalization.
+
+This script showcases the use of Python Dicts to determine the
+bank names associated with the corresponding market cap ranges.
+"""
+
+# Banks and Market Caps
+#-----------------------
+# National Bank of Canada: 327
+# Toronto-Dominion Bank: 302
+# Royal Bank of Canada: 173
+# Wells Fargo: 273
+# Goldman Sachs: 87
+# Morgan Stanley: 72
+# Canadian Imperial Bank of Commerce: 83
+# TD Bank: 108
+# Bank of Montreal: 67
+# Capital One: 47
+# FNB Corporation: 4
+# Laurentian Bank of Canada: 3
+# Ally Financial: 12
+# Montreal Trust Company: 145
+# Canadian Western Bank: .97
+
+# @TODO: Initialize a dictionary of banks and market caps (in billions)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# @TODO: Change the market cap for 'Royal Bank of Canada'
+
+
+# @TODO: Add a new bank and market cap pair
+
+
+# @TODO: Remove a bank from the dictionary
+
+
+# @TODO: Initialize metric variables
+
+
+
+# @TODO: Initialize minimum key-value pair
+
+
+
+# @TODO: Initialize maximum key-value pair
+
+
+
+
+# Mega Cap: Firms with a market capitalization over $300 billion.
+# Large Cap: Firms with a market capitalization over 10 billion. ...
+# Mid Cap: A market capitalization between $2 and $10 billion.
+# Small Cap: A market capitalization between $300 million and $2 billion.
+
+# @TODO: Initialize market cap lists
+
+
+
+
+
+# @TODO: Iterate over key-value pairs of the dictionary
+
+
+
+
+    # @TODO: Calculate sum of market caps and number of banks in the dictionary
+
+
+
+    # @TODO: Logic to determine min value and associated key
+
+
+
+
+    # @TODO: Logic to determine max value and associated key
+
+
+
+
+    # @TODO: Group banks by categories of market caps
+
+
+
+
+# @TODO: Calculate average market cap of banks in the dictionary
+
+
+
+# @TODO: Print the metrics
---
+++ week02/class2/trading_log.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+"""
+Student Do: Trading Log.
+
+This script demonstrates how to perform basic analysis of trading profits/losses
+over the course of a month (20 business days).
+"""
+
+# @TODO: Initialize the metric variables
+
+total = 0
+count = 0
+maximum = 0
+minimum = 0
+profitable_days = []
+unprofitable_days = []
+
+
+# @TODO: Initialize lists to hold profitable and unprofitable day profits/losses
+
+
+
+
+# List of trading profits/losses
+trading_pnl = [ -224,  352, 252, 354, -544,
+                -650,   56, 123, -43,  254,
+                 325, -123,  47, 321,  123,
+                 133, -151, 613, 232, -311 ]
+
+# @TODO: Iterate over each element of the list
+
+
+    # @TODO: Cumulatively sum up the total and count
+
+
+    # @TODO: Write logic to determine minimum and maximum values
+
+
+
+
+
+
+    # @TODO: Write logic to determine profitable vs. unprofitable days
+
+
+
+
+
+# @TODO: Calculate the average
+
+
+# @TODO: Calculate count metrics
+
+
+# @TODO: Calculate percentage metrics
+
+
+
+# @TODO: Print out the summary statistics
+
