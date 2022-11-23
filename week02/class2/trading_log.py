bf2f6fc3e201e45a7c4b741afd37ac2f655895a0
jiy.juil@gmail.com <jiy.juil@gmail.com>
2022-11-22 20:14:47 -0500

    All calculations. No output

--- week02/class2/trading_log.py
+++ week02/class2/trading_log.py
@@ -17,7 +17,7 @@ minimum = 0
 # @TODO: Initialize lists to hold profitable and unprofitable day profits/losses
 
 profitable_days = []
-unprofitable_days = []
+unprofitable_days = [] 
 
 
 # List of trading profits/losses
@@ -28,29 +28,30 @@ trading_pnl = [ -224,  352, 252, 354, -544,
 
 # @TODO: Iterate over each element of the list
 
-for i in range(trading_pnl):
+for i in range(len(trading_pnl)):
     # @TODO: Cumulatively sum up the total and count
     total += trading_pnl[i]
     count += 1
 
     # @TODO: Write logic to determine minimum and maximum values
-
-
-
-
-
+    if trading_pnl[i] < minimum:
+        minimum = trading_pnl[i]
+    elif trading_pnl[i] > maximum:
+        maximum = trading_pnl[i]
 
     # @TODO: Write logic to determine profitable vs. unprofitable days
-
-
-
-
+    if trading_pnl[i] > 0:
+        profitable_days.append(trading_pnl[i]) 
+    elif trading_pnl[i] < 0:
+        unprofitable_days.append(trading_pnl[i])
 
 # @TODO: Calculate the average
-
+total_pnl = sum(trading_pnl)
+average = total_pnl/len(trading_pnl)
+print(f'Average PNL: ${average:.2f}')
 
 # @TODO: Calculate count metrics
-
+print('**Summary Statistics**')
 
 # @TODO: Calculate percentage metrics
 
