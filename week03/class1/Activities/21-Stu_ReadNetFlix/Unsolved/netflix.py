af258376178dc1b66bbdd9c15b52af1558249878
jiy.juil@gmail.com <jiy.juil@gmail.com>
2022-11-29 13:58:58 -0500

    Database search

--- week03/class1/Activities/21-Stu_ReadNetFlix/Unsolved/netflix.py
+++ week03/class1/Activities/21-Stu_ReadNetFlix/Unsolved/netflix.py
@@ -1,13 +1,33 @@
 # Modules
 import os
+from pathlib import Path
 import csv
+import pandas as pd
 
 # Prompt user for video lookup
-
+lookup = input("Search Netflix: ")
 
 # Set path for CSV file
-
+path = "../Resources/netflix_ratings.csv"
 
 # Open the file and loop through to search for the video
+# netflixfile = os.open(path,os.O_RDONLY)
 # If video is found, print title, rating, and user ratings. 
 # If the video is never found, alert the user
+
+csvreader = csv.reader(path,delimiter=',')
+
+with open(path,'r') as netflix:
+    fields = next(netflix).split(', ')
+    print(fields)
+    movie = False
+    for i in netflix:
+        if lookup.lower() in i.split(', ')[0].lower():
+            print(i)
+            movie = True
+    if not movie:
+        print(f"{lookup} not found.")
+
+# os.close(netflixfile)
+dataframe = pd.read_csv(path)
+print(dataframe)
