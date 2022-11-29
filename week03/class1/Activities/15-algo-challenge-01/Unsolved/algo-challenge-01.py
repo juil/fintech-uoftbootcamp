722c5450bde50d8ba18344271c52ef7177c503ce
jiy.juil@gmail.com <jiy.juil@gmail.com>
2022-11-28 21:02:46 -0500

    Algorithms!

--- week03/class1/Activities/15-algo-challenge-01/Unsolved/algo-challenge-01.py
+++ week03/class1/Activities/15-algo-challenge-01/Unsolved/algo-challenge-01.py
@@ -1,4 +1,5 @@
 # Import the `random` library.
+import random
 
 # Answer each question with the correct coding solution.
 
@@ -6,12 +7,18 @@
 # If the number is 42, print(true). If it isn't 42, print(false)
 
 # YOUR CODE HERE
+def number_guess(num):
+    print(int(num) == 42)
 
+number_guess(input("Guess: "))
 
 # QUESTION 2: Write a function that takes in a list of numbers. the function should print the smallest number in the given list
 
 # YOUR CODE HERE
+def smallest(numlist):
+    print(min(numlist))
 
+smallest([1,2,3,4])
 
 # QUESTION 3: Write a function which takes in a list of strings. The function should print the shortest string in the list.
 
@@ -22,3 +29,12 @@
 # The function should print a new list of numbers where the elements are greater than the low value and less than the high value
 
 # YOUR CODE HERE
+def between(high, low, numlist):
+    newlist = []
+    for i in numlist:
+        if low < i and i < high:
+            newlist.append(i)
+    print(newlist)
+
+nums = [1,2,3,4,5,6,6,7,8]
+between(5,2,nums)
