22b7fbcbe28ff12c21661124b4205ceb529500a9
jiy.juil@gmail.com <jiy.juil@gmail.com>
2023-01-06 14:00:04 -0500

    Import data and query based on gender and state

--- 07-sql/class1/activities/07-Stu_Customer_Demographics/Unsolved/script.sql
+++ 07-sql/class1/activities/07-Stu_Customer_Demographics/Unsolved/script.sql
@@ -13,3 +13,17 @@ create table customer (
 	state char(2),
 	zip_code char(6)
 );
+
+-- Copy data from CSV file
+-- #TODO: Figure out user access permission for folder/file
+copy customer from '/Users/juil/school/fintech-uoftbootcamp/classwork/07-sql/class1/activities/07-Stu_Customer_Demographics/Resources/customer.csv' with (FORMAT csv);
+
+select * from customer;
+
+-- Get all female customers
+select first_name, last_name from customer
+where gender = 'Female';
+
+-- Query: male customers from New Jersey or Ohio
+select first_name, last_name, state from customer
+where gender = 'Male' and state = 'NJ' or state = 'OH';
