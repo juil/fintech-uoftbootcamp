6b1c1900df23d0196e5d78c5b282463562312d19
jiy.juil@gmail.com <jiy.juil@gmail.com>
2023-01-06 00:55:07 -0500

    Create table and sql edit

---
+++ 07-sql/class1/activities/05-Stu_Making_IDs/Unsolved/query.sql
@@ -0,0 +1,56 @@
+-- Schema
+create table banks (
+	bank_id SERIAL primary key,
+	bank_name VARCHAR(50),
+	bank_routing_number BIGINT
+);
+
+drop table banks;
+
+-- Seed
+-- Insert new data
+INSERT INTO banks 
+(bank_name, bank_routing_number)
+VALUES
+('Bank of America', 198491827),
+('Wells Fargo', 629873495),
+('JPMorgan Chase', 2340903984),
+('Citigroup', 890123900),
+('TD Bank', 905192010),
+('Capital One', 184619239),
+('Capital One', 184619239);
+
+select * from banks
+
+select * from banks 
+where bank_name = 'Capital One';
+
+delete from banks 
+where bank_id = 7;
+
+select * from banks;
+
+-- Add banks
+insert into banks 
+	(bank_name, bank_routing_number)
+values 
+	('CIBC', 010),
+	('TD Bank', 103);
+	
+select * from banks;
+
+-- Change values
+update banks
+set bank_name = 'PNC Bank'
+where bank_name = 'Citigroup';
+
+select * from banks;
+
+update banks 
+set bank_routing_number = 1995826182
+where bank_name = 'Wells Fargo';
+
+select * from banks;
+
+-- Add column to table with default value
+
