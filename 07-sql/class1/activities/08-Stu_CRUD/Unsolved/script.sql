c5432b91870fc200adccee9de1d4b8cc661a1b71
jiy.juil@gmail.com <jiy.juil@gmail.com>
2023-01-06 14:24:52 -0500

    Create table

---
+++ 07-sql/class1/activities/08-Stu_CRUD/Unsolved/script.sql
@@ -0,0 +1,18 @@
+-- sales_db
+-- Create table
+-- Drop table if exists
+DROP TABLE IF EXISTS sales;
+
+-- Create new table to import data
+CREATE TABLE sales (
+  sales_id SERIAL PRIMARY KEY,
+  payment_id INT,
+  mortgage_id INT,
+  loan_amount INT,
+  loan_date DATE
+);
+
+-- Return records with loan amounts less than $300,000
+select sales_id, loan_amount from sales
+where loan_amount > 300000;
+
