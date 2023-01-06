3c9466518c43649400cfb95c02d496db387df9cc
jiy.juil@gmail.com <jiy.juil@gmail.com>
2023-01-06 16:17:26 -0500

    Update data

--- 07-sql/class1/activities/08-Stu_CRUD/Unsolved/script.sql
+++ 07-sql/class1/activities/08-Stu_CRUD/Unsolved/script.sql
@@ -12,9 +12,20 @@ CREATE TABLE sales (
   loan_date DATE
 );
 
+-- Load table data 
+copy sales from '/Users/juil/school/fintech-uoftbootcamp/classwork/07-sql/class1/activities/08-Stu_CRUD/Resources/sales.csv' with (FORMAT csv);
+
 -- Return records with loan amounts less than $300,000
 select sales_id, loan_amount from sales
 where loan_amount > 300000;
 
 -- Find the average loan amount of all sales records
 select avg(loan_amount) from sales;
+
+-- Update sales_id 33
+select sales_id, loan_amount from sales
+where sales_id = 33;
+
+update sales 
+set loan_amount = 423212
+where sales_id = 33;
