8c037e0489831101a842ea754b1159fd8f98a594
jiy.juil@gmail.com <jiy.juil@gmail.com>
2023-01-09 20:43:51 -0500

    Aggregate queries.

--- 07-sql/class2/Activities/03-Stu_GregariousAggregates/Unsolved/query.sql
+++ 07-sql/class2/Activities/03-Stu_GregariousAggregates/Unsolved/query.sql
@@ -1,20 +1,28 @@
---1. What is the average payment amount?
+select * from payment;
 
+--1. What is the average payment amount?
+select avg(amount) from payment; 
 
 --2. What is the total payment amount?
-
+select sum(amount) from payment;
 
 --3. What is the minimum payment amount?
-
+select min(amount) from payment; 
 
 --4. What is the maximum payment amount?
-
+select max(amount) from payment;
 
 --5. How many customers has each staff serviced?
-
+select staff_id, count(customer_id) from payment
+group by staff_id;
 
 --6. What is the count of payments for each customer?
-
+select customer_id, count(payment_id) as "count_payments"
+from payment
+group by customer_id;
 
 --7. Which customers have made over 40 payments?
-
+select customer_id, count(payment_id) as count_payments
+from payment
+group by customer_id
+having count(payment_id) > 40;
