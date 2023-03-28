a6c80474539daa1ac02c80a009754a7fac40fd17
Juil <dev@juil.me>
2023-03-27 21:13:32 -0400

    Create streamlit page

--- 18-blockchain/class1/Activities/04-Stu_A_Streamlit_App/Unsolved/app.py
+++ 18-blockchain/class1/Activities/04-Stu_A_Streamlit_App/Unsolved/app.py
@@ -1,28 +1,32 @@
 # @TODO: Import the libraries for Pandas and Streamlit
-# YOUR CODE HERE!
+import streamlit as st
+import pandas as pd
 
 # @TODO: Create a title for your application using markdown syntax and the
 # Streamlit `write` function.
-# YOUR CODE HERE!
+st.write("# Streamlit Application")
 
 # @TODO: Create an opening sentence for your application using regular text and
 # the Streamlit `write` function.
-# YOUR CODE HERE!
+st.write("We are **LIVE**")
 
 # @TODO: Create a Pandas dataframe
-# YOUR CODE HERE!
+df = pd.DataFrame({'name': ['Juil', 'Yogurt', 'Conda']})
 
 # @TODO: Write the Pandas dataframe to the page
-# YOUR CODE HERE!
+st.write(df)
 
 # @TODO: Create a text input box using the Streamlit `text_input` function.
 # @TODO: Save the input as a variable.
-# YOUR CODE HERE!
+new_username = st.text_input("User Name:")
 
 # @TODO: Utilize the Streamlit `button` function to display the text input
 # variable created in the prior step to the page.
-# YOUR CODE HERE!
-
+if st.button("Add"):
+    new_user = {'name': new_username}
+    df.loc[len(df)] = new_user
+    st.write(f"{new_username} added.")
+    st.write(df)
 
 # Bonus
 # YOUR CODE HERE!
