f77888e67a1ec6a5f84f03d28c166d8f305f587e
Juil <dev@juil.me>
2023-03-28 20:58:07 -0400

    Block hashing function

--- 18-blockchain/class2/Activities/06-Stu_Hashing_a_Block/Unsolved/app.py
+++ 18-blockchain/class2/Activities/06-Stu_Hashing_a_Block/Unsolved/app.py
@@ -38,33 +38,34 @@ class Block:
 
     # @TODO:
     # Add a new function called `hash_block`
-    # YOUR CODE HERE!
+    def hash_block(self):
 
-    # @TODO:
-    # Add an instance of the `sha256` hashing function
-    # YOUR CODE HERE!
+        # @TODO:
+        # Add an instance of the `sha256` hashing function
+        sha = hashlib.sha256()
 
-    # @TODO:
-    # Encode the Block's data attribute
-    data =  # YOUR CODE HERE!
-    # Update the encoded data using the hashing function
-    # YOUR CODE HERE!
+        # @TODO:
+        # Encode the Block's data attribute
+        data =  str(self.data).encode()
+        # Update the encoded data using the hashing function
+        sha.update(data)
 
-    # @TODO:
-    # Encode the Blocks's creator_id attribute
-    creator_id =  # YOUR CODE HERE!
-    # Update the encoded creator_id using the hashing function
-    # YOUR CODE HERE!
+        # @TODO:
+        # Encode the Blocks's creator_id attribute
+        creator_id =  str(self.creator_id).encode()
+        # Update the encoded creator_id using the hashing function
+        sha.update(creator_id)
 
-    # @TODO:
-    # Encode the Block's timestamp attribute
-    timestamp =  # YOUR CODE HERE!
-    # Update the encoded timestamp using the hashing function
-    # YOUR CODE HERE!
+        # @TODO:
+        # Encode the Block's timestamp attribute
+        timestamp =  str(self.timestamp).encode()
+        # Update the encoded timestamp using the hashing function
+        sha.update(timestamp)
 
-    # @TODO:
-    # Return the hashes of all the Block class attributes
-    # YOUR CODE HERE!
+        # @TODO:
+        # Return the hashes of all the Block class attributes
+        return sha.hexdigest()
+    
 
 
 ################################################################################
@@ -93,11 +94,11 @@ if st.button("Add Block"):
 
     # @TODO:
     # Call the `hash_block` function on the `new_block` to create a `block_hash`
-    block_hash =  # YOUR CODE HERE!
+    block_hash =  new_block.hash_block()
 
     # @TODO:
     # Use `st.write` to display the `block_hash` to the page.
-    # YOUR CODE HERE!
+    st.write(block_hash)
 
 
 ################################################################################
