e572a6bd8937a367e8766b1f420538a1048f25a2
Juil <dev@juil.me>
2023-04-18 20:08:56 -0400

    Create MNSTR ArcadeToke

--- 21-advanced_solidity/class1/Activities/02-Stu_Arcade_Token/Unsolved/ArcadeToken.sol
+++ 21-advanced_solidity/class1/Activities/02-Stu_Arcade_Token/Unsolved/ArcadeToken.sol
@@ -3,3 +3,38 @@ Arcade Token
 */
 
 // Construct your ArcadeToken contract below:
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8;
+
+contract ArcadeToken {
+    address owner = msg.sender;
+    bytes5 public symbol = "MNSTR";
+    uint128 public exchange_rate = 1 * 10**18;
+    
+    mapping(address => uint) public balances;
+
+    function balance() public view returns (uint) {
+        return balances[msg.sender];
+    }
+
+    function transfer(address recipient, uint value) public {
+        balances[msg.sender] -= value;
+        balances[recipient] += value;
+    }
+
+    function purchase() public payable {
+        uint _value = msg.value;
+        uint amount = _value * exchange_rate;
+        balances[msg.sender] += amount;
+        // Send value to owner?
+        payable(owner).transfer(_value);
+    }
+
+    function mint(address recipient, uint value) public {
+        require(msg.sender==owner, "Only the owner can mint tokens.");
+        balances[recipient] += value;
+    }
+
+
+}
